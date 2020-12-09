a56d3a44c5b13c2efe57a5f1ec3658e5b621cbdd
simplehuey <75712196+simplehuey@users.noreply.github.com>
2020-12-09 21:24:56 +0800

    Update hw1.1.py

--- hw1.1.py
+++ hw1.1.py
@@ -1,7 +1,7 @@
 # 買 x1 張全票與 x2 張學生票，而一張全票的售價是 p1 元，一張學生票則是 p2 元。
 # 若你拿出 t 元鈔票給櫃臺，請問是否足夠，以及若足夠，櫃臺會找你多少錢？
 # 判斷 t 是否足夠支付票價，如果不夠則印出一個 −1；夠則先印出一個錢字號「$」，再印出櫃台找錢的金額，中間不可以有任何其他字元（包括空白字元）。
-
+# hi
 adult_ticket_quantity = int(input())
 adult_ticket_price = int(input())
 student_ticket_quantity = int(input())
